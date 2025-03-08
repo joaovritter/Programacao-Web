a7e89332de18ca8dea71d2a4b94e9f02973a9f7c
João Vitor dos Santos Ritter <125499584+joaovritter@users.noreply.github.com>
2025-03-07 21:34:07 -0300

    Add files via upload

--- EmpresaJPA - Aula 06.03/src/main/java/com/joaozaobala/empresajpa/Dependente.java
+++ EmpresaJPA - Aula 06.03/src/main/java/com/joaozaobala/empresajpa/Dependente.java
@@ -7,12 +7,18 @@ import java.util.Date;
 @Entity
 public class Dependente {
     @Id
-    @EmbeddedId
+    @EmbeddedId //chave primaria composta que vem do DependenteId
     private DependenteId id;
     private String sexo;
     private Date dataNascimento;
-    @ManyToOne
+
+
+
+    //MapsID: A partir do parentesco ele descobre a chave estrangeira (cpf do funcionario)
+    // mapeia a chave estrangeira (Fcpf) diretamente na chave primária composta (DependenteId)
+    // depois ele joga o Fpf para a minha chave primaria, como ela é composta, especifico que é no funcionario_cpf
     @JoinColumn (name= "Fcpf")
     @MapsId ("funcionario_cpf")
+    @ManyToOne
     private Funcionario parentesco;
 }
