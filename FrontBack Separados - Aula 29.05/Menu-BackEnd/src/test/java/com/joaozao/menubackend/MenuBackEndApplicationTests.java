81785967b3e970913e2a59c96456b244e20fa099
João Vitor dos Santos Ritter <joao.ritter@ufn.edu.br>
2025-05-29 21:55:33 -0300

    criacao do front end

---
+++ FrontBack Separados - Aula 29.05/Menu-BackEnd/src/test/java/com/joaozao/menubackend/MenuBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaozao.menubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ FrontBack Separados - Aula 29.05/Menu-FrontEnd/src/App.tsx
@@ -0,0 +1,12 @@
+import {ProdutoLista} from "./components/ProdutoLista.tsx";
+
+function App() {
+
+  return (
+    <>
+      <ProdutoLista/>
+    </>
+  )
+}
+
+export default App
